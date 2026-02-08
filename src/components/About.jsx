ac69f2fd8cb1b2eb60636cd04bd2a0864a2e4bc2
Atul <sleekatul@gmail.com>
2026-02-08 20:31:26 +0530

    Add 3 more tools: Entity Framework, Postman, and Docker

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,9 @@ export default function About() {
     { name: 'maui', icon: './assets/maui.webp' },
     { name: 'azure', icon: './assets/azure.svg' },
     { name: 'git', icon: './assets/git.svg' },
+        { name: 'entityframework', icon: './assets/ef.png' },
+        { name: 'postman', icon: './assets/postman.png' },
+        { name: 'docker', icon: './assets/docker.png' },
   ];
 
   const data = [
