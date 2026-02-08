41353846ed72c911fc899f82d8a9ce055f0076ba
Atul <sleekatul@gmail.com>
2026-02-08 16:51:37 +0530

    updated arrow icon

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -161,8 +161,8 @@ export default function Work() {
           className="p-3 p-3 bg-white border-2 border-gray-700 rounded-full hover:bg-gray-100 dark:bg-gray-800 dark:border-white
           aria-label="Previous project"
         >
-          <img src="./assets/right-arrow.png" alt="" className="w-4 dark:hidden" style={{transform: 'rotate(180deg)'}} />
-          <img src="./assets/right-arrow-dark.png" alt="" className="w-4 hidden dark:block" />
+          <img src="./assets/right-arrow.png" alt="" className="w-4 dark:hidden" style={{ transform: 'rotate(180deg)' }} />
+          <img src="./assets/right-arrow-dark.png" alt="" className="w-4 hidden dark:block" style={{ transform: 'rotate(180deg)' }} />
         </button>
         <button
           onClick={nextProject}
