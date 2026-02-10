fa9beca6086149232974df66a93dee70bfd0d5a7
Atul <sleekatul@gmail.com>
2026-02-10 17:01:02 +0530

    Updated text style for project card

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -386,10 +386,10 @@ export default function Work() {
               }
             }}
           >
-            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+            <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
               <div>
                 <h2 className="font-semibold">{project.title}</h2>
-                <p className="text-sm text-gray-700">{project.category}</p>
+                <p className="text-sm text-gray-700 dark:text-gray-300">{project.category}</p>
               </div>
               <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
                 <img src="./assets/send-icon.png" alt="" className="w-5" />
